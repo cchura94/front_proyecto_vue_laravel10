0039516331e90ca6f41d5d397683c31eca780d74
cristian <cchura.cpc@gmail.com>
2023-06-27 22:53:32 -0400

    Login Auth Vue Interceptores

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,31 +4,53 @@
         <h1>Login</h1>
         <label for="e">Ingresar Correo:</label>
         <input type="email" v-model="usuario.email">
+        <span>{{ errors.email }}</span>
         <br>
         <label>Ingresar Contraseña:</label>
         <input type="password" v-model="usuario.password">
+        <span>{{ errors.password }}</span>
         <br>
         <input type="button" value="Ingresar" v-on:click="funIngresar()">
+
 
     </div>
 </template>
 
 <script setup>
     import { ref } from "vue";
-    import axios from "axios"
+    import axios from "axios";
+    import { useRouter } from 'vue-router'
+    import authService from "./../services/auth.service"
 
     // variables
     // const correo = ref("micorreo@mail.com");
     // const clave = ref("mipassword");
     const usuario = ref({email: "", password: ""})
+    const errors = ref({})
+
+    const router = useRouter()
 
     // metodos
-    function funIngresar(){
+    async function funIngresar(){
+        try {
+            const { data } = await authService.loginConLaravel(usuario.value)
+            console.log("CON INTERCEPTOR: ", data)
+            errors.value = {}
+
+            localStorage.setItem("access_token", data.access_token);
+            router.push("/about")
+           
+        } catch (error) {
+            console.log(error.response.data)
+            errors.value = error.response.data.errors
+        }
+
+        /*
         axios.post("http://127.0.0.1:8000/api/v1/auth/login", usuario.value).then(
             (res) => {
-                console.log(res)
+                console.log("CON AXIOS: ", res.data)
             }
-        )
+        )*/
     }
 
 </script>
